400d947df3712ecaf51d48e94895a74d9f6fdd2b
egor antrop <mougraaa@gmail.com>
2023-09-01 23:49:50 +0400

    feat: footerm sidebar

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
+import styles from '../../styles/Footer.module.css'
+
+import { ROUTES } from '../../utils/routes'
+import { Link } from 'react-router-dom'
+
+import LOGO from '../../images/logo.svg'
+
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <section className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt='stuff' />
+        </Link>
+      </div>
+      <div className={styles.rights}>
+        Developed by{' '}
+        <a href='https://github.com/mougra' target='_blank' rel='noreferrer'>
+          mougra
+        </a>
+      </div>
+      <div className={styles.socials}>
+        <a href='https://github.com/mougra' target='_blank' rel='noreferrer'>
+          <svg className='icon'>
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+          </svg>
+        </a>
+      </div>
+    </section>
+  )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
+import styles from '../../styles/Sidebar.module.css'
+import { NavLink } from 'react-router-dom'
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href='/help' className={styles.link} target='_blank'>
+          Help
+        </a>
+        <a
+          href='/terms'
+          className={styles.link}
+          target='_blank'
+          style={{ textDecoration: 'underline' }}
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  )
 }
